73b5412c470b8ae5588f150c4842a59ae21028f0
Yurii Matora <yura.matora11@gmail.com>
2018-12-16 21:32:51 +0200

    Add args control to client/server

--- src/com/filesync/Main.java
+++ src/com/filesync/Main.java
@@ -1,16 +1,15 @@
 package com.filesync;
 
 import java.io.*;
-import java.util.Date;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
         if(args[0].equals("-s")) {
-            Server server = new Server(4646, "./ServerFolder");
+            Server server = new Server(4646, args[1]);
             server.startServer();
         } else if(args[0].equals("-c")){
-            Client client = new Client("localhost", 4646, "./ClientFolder");
+            Client client = new Client(args[1], 4646, args[2]);
             client.startClient();
             System.exit(0);
         }
